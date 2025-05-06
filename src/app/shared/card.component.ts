ee091928caa8c7d39682626fdd9b43b5d2585d8d
bansheett <avecesare01@gmail.com>
2025-05-06 17:47:06 +0200

    Quarto commit

---
+++ src/app/shared/card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+@Component({
+    selector: "app-card",
+    standalone: true,
+    imports: [FormsModule],
+    templateUrl: "./card.component.html",
+    styleUrl: "./card.component.css",
+})
+export class CardComponent {
+
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,19 +1,17 @@
 import { Component, EventEmitter, Input, Output} from "@angular/core";
-
-type User = {
-    id: string;
-    avatar: string;
-    name: string;
-}
+import { type User } from "./user.model";
+import { CardComponent } from "../shared/card.component";
 
 @Component({
     selector: "app-user",
     standalone: true,
     templateUrl: "./user.component.html",
     styleUrl: "./user.component.css",
+    imports: [CardComponent],
 }) 
 export class UserComponent {
     @Input({required:true}) user!: User;
+    @Input({required:true}) selected!: boolean;
     @Output() select = new EventEmitter();
     // l'altra alternativa:
     //select = output<string>();
