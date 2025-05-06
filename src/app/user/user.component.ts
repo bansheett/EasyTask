2252f2d6f9f29d777983fb2ae6284464a0079df5
bansheett <avecesare01@gmail.com>
2025-05-06 12:15:34 +0200

    third commit

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Input, Output} from "@angular/core";
+
+type User = {
+    id: string;
+    avatar: string;
+    name: string;
+}
 
 @Component({
     selector: "app-user",
@@ -7,17 +13,17 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
     styleUrl: "./user.component.css",
 }) 
 export class UserComponent {
-    @Input({required: true}) id!: string;
-    @Input({required: true}) avatar!: string;
-    @Input({required: true}) name!: string;
+    @Input({required:true}) user!: User;
     @Output() select = new EventEmitter();
+    // l'altra alternativa:
+    //select = output<string>();
 
     get imagePath() {
-        return "assets/users/" + this.avatar;
+        return "assets/users/" + this.user.avatar;
     }
 
     onSelectUser() {
-        this.select.emit(this.id);
+        this.select.emit(this.user.id);
     }
 
     // avatar e name grazie al decorator "input" sono ora dei segnali.
